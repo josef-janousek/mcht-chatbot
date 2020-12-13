f811390cf82f8eb2f10221db3d8e1c4aac07384f
Josef Janoušek <pauto@centrum.cz>
2020-12-13 15:39:08 +0100

    initial commit - chatbot

---
+++ index.js
@@ -0,0 +1,21 @@
+const BootBot = require('bootbot');
+const config = require('config');
+
+const port = process.env.PORT || config.get('PORT');
+
+const bot = new BootBot({
+  accessToken: config.get('ACCESS_TOKEN'),
+  verifyToken: config.get('VERIFY_TOKEN'),
+  appSecret: config.get('APP_SECRET'),
+});
+
+bot.on('message', (payload, chat) => {
+  const text = payload.message.text;
+  console.log(`The user said: ${text}`);
+});
+
+bot.hear(['hello', 'hi'], (payload, chat) => {
+  console.log('The user said "hello" or "hi"!');
+});
+
+bot.start(port);
